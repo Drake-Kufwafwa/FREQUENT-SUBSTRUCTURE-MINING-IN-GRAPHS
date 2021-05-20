b43032435191a5025143bdde6619ae825d82e464
Paul Jackson <61030352+pjackson31@users.noreply.github.com>
2021-05-20 10:58:34 -0400

    Update graphRandom.py

--- graphRandom.py
+++ graphRandom.py
@@ -16,16 +16,16 @@ class graphRandom:
     
     def allGraphs(self):
         #loading of ten graphs to mine - Im sure there is faster way to go across the data set #.edges gives ego network - how many friendships person has
-        self.G1 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G2 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G3 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G4 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G5 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G6 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G7 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G8 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G9 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
-        self.G10 = snap.GenRndGnm(snap.PNGraph, 10000, 5000,False)
+        self.G1 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G2 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G3 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G4 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G5 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G6 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G7 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G8 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G9 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
+        self.G10 = snap.GenRndGnm(snap.TUNGraph, 10000, 5000,False)
     
         self.graphs=[self.G1,self.G2,self.G3,self.G4,self.G5,self.G6,self.G7,self.G8,self.G9,self.G10] ##
     
